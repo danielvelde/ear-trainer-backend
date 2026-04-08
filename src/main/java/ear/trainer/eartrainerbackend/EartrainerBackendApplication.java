6bf3405dc10bfd373999909ff9b402ecd417d225
Daniel <danielvdvelde@proton.me>
2026-04-08 14:24:24 +0200

    First commit

---
+++ src/main/java/ear/trainer/eartrainerbackend/EartrainerBackendApplication.java
@@ -0,0 +1,13 @@
+package ear.trainer.eartrainerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EartrainerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EartrainerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ear/trainer/eartrainerbackend/EartrainerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ear.trainer.eartrainerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EartrainerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
