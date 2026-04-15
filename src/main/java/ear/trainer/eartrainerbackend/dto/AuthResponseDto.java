7826d725abd714409776cb644bf23db81089a751
Daniel <danielvdvelde@proton.me>
2026-04-15 14:21:57 +0200

    New branch focussing primarily on storing the jwt / cookies

--- src/main/java/ear/trainer/eartrainerbackend/dto/AuthResponseDto.java
+++ src/main/java/ear/trainer/eartrainerbackend/dto/AuthResponseDto.java
@@ -1,7 +1,11 @@
 package ear.trainer.eartrainerbackend.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class AuthResponseDto {
     @JsonProperty("access_token")
     private String accessToken;
