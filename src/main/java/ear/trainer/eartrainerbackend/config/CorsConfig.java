8e4fdfdfaac78492b2779a2d972169708f91ad5b
Daniel <danielvdvelde@proton.me>
2026-04-16 15:08:55 +0200

    Variable for cors allowed-origins now specifiet in application.yaml

--- src/main/java/ear/trainer/eartrainerbackend/config/CorsConfig.java
+++ src/main/java/ear/trainer/eartrainerbackend/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package ear.trainer.eartrainerbackend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,13 +9,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
+    @Value("${cors.allowed-origins}")
+    private String allowedOrigins;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/auth/**")
-                        .allowedOrigins("http://localhost:5173")   // your Vite dev server
+                        .allowedOrigins(allowedOrigins)
+
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true)
