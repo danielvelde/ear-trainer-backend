fc24d40520e89b81c9386cae098e9195df782969
Daniel <danielvdvelde@proton.me>
2026-04-21 14:06:50 +0200

    Forgot to git add .

--- src/main/java/ear/trainer/eartrainerbackend/dto/FreesoundRequestDto.java
+++ src/main/java/ear/trainer/eartrainerbackend/dto/FreesoundRequestDto.java
@@ -1,4 +1,10 @@
 package ear.trainer.eartrainerbackend.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class FreesoundRequestDto {
+    private String query;
 }
--- src/main/java/ear/trainer/eartrainerbackend/dto/FreesoundResponseDto.java
+++ src/main/java/ear/trainer/eartrainerbackend/dto/FreesoundResponseDto.java
@@ -1,4 +1,11 @@
 package ear.trainer.eartrainerbackend.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class FreesoundResponseDto {
+    private String id;
+    private String name;
 }
